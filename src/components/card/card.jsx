c3ebc4b0cfeb36b58f28699d2ce63d5bb5cd9faf
Emil <somaleyoo123@gmail.com>
2025-05-07 14:40:55 +0200

    style card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,20 +6,24 @@ export default function Card() {
             <section id='card'>
                 <div className='cards-container'>
                     <div className="card">
+                        <div className="img-container">
                         <img src="../../src/assets/images/image-equilibrium.jpg" className='card-img' alt="" />
                             <div className='overlay'>
-                                <img src="../../src/assets/images/icon-view.svg" alt="" />
+                                <img src="../../src/assets/images/icon-view.svg" className='icon1' alt="" />
+                                </div>
                             </div>
+                            <div className="card-body">
                         <h5>Equilibrium #3429</h5>
                         <p>Our Equilibrium collection promotes balance and calm.</p>
                         <div className='info'>
-                            <p><span><img src="../../src/assets/images/icon-ethereum.svg" className='icon' alt="" />0.041ETH</span></p>
+                            <p><span className='cyan'><img src="../../src/assets/images/icon-ethereum.svg" className='icon' alt="" />0.041ETH</span></p>
                             <p><span><img src="../../src/assets/images/icon-clock .svg" className='icon' alt="" />3 days left</span></p>
                         </div>
                         <hr />
                         <div className="profile">
                             <img src="../../src/assets/images/image-avatar.png" alt="" className="pfp" />
                             <p>Creation of <span className='autor'>Jules Wyvern</span></p>
+                        </div>
                         </div>
                     </div>
                 </div>
