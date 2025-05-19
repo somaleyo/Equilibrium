f7d27298540073063f4b899f6ef47fc3ccbba156
Emil <somaleyoo123@gmail.com>
2025-05-19 10:04:11 +0200

    pages'

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,5 +1,9 @@
 import './card.css'
-
+import equi from '/src/images/image-equilibrium.jpg'
+import hover from '/src/assets/images/icon-view.svg'
+import left from '/src/assets/images/icon-ethereum.svg'
+import clock from '/src/assets/images/icon-clock.svg'
+import pfp from '/src/assets/images/image-avatar.png'
 export default function Card() {
     return (
         <>
@@ -7,21 +11,21 @@ export default function Card() {
                 <div className='cards-container'>
                     <div className="card">
                         <div className="img-container">
-                        <img src="../../src/assets/images/image-equilibrium.jpg" className='card-img' alt="" />
+                        <img src={equi} className='card-img' alt="" />
                             <div className='overlay'>
-                                <img src="../../src/assets/images/icon-view.svg" className='icon1' alt="" />
+                                <img src={hover} className='icon1' alt="" />
                                 </div>
                             </div>
                             <div className="card-body">
                         <h5>Equilibrium #3429</h5>
                         <p>Our Equilibrium collection promotes balance and calm.</p>
                         <div className='info'>
-                            <p><span className='cyan'><img src="../../src/assets/images/icon-ethereum.svg" className='icon' alt="" />0.041ETH</span></p>
-                            <p><span><img src="../../src/assets/images/icon-clock .svg" className='icon' alt="" />3 days left</span></p>
+                            <p><span className='cyan'><img src={left} className='icon' alt="" />0.041ETH</span></p>
+                            <p><span><img src={clock} className='icon' alt="" />3 days left</span></p>
                         </div>
                         <hr />
                         <div className="profile">
-                            <img src="../../src/assets/images/image-avatar.png" alt="" className="pfp" />
+                            <img src={pfp} alt="" className="pfp" />
                             <p>Creation of <span className='autor'>Jules Wyvern</span></p>
                         </div>
                         </div>
